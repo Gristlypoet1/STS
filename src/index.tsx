150deef0ea91c488a17a8ae094f5820c8702bf0b
Gristlypoet1 <kverherstraeten@outlook.com>
2025-06-22 16:23:43 +0200

    modified:   src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles/App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
